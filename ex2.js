020456311b827ceadf633c8778b33c1eb4f88c3a
ricardo <rcavaraujo@gmail.com>
2022-07-13 19:25:52 -0300

    [M1S03] Ex 2 - Iterando sobre arrays de objetos

---
+++ ex2.js
@@ -0,0 +1,21 @@
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+  ];
+
+function itera() {
+    listaConvidados.forEach(function(objeto) {
+        document.write("<li>" + objeto.nome + " " + objeto.sobrenome + "</li>");
+      })
+}
+
+
+ 
