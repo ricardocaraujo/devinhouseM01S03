046b7116a7d9942e45c488f5335d947a507b952a
ricardo <rcavaraujo@gmail.com>
2022-07-17 20:53:59 -0300

    [M1S03] Ex 4 - Projeto: iniciando

---
+++ ex4.js
@@ -0,0 +1,20 @@
+var convidado1 = {
+    nome: "Ricardo",
+    sobrenome: "Araujo",
+    setor: "camarote",
+    idade: 39
+}
+
+var convidado2 = {
+    nome: "João",
+    sobrenome: "Martins",
+    setor: "arquibancada",
+    pista: 16
+}
+
+var convidado3 = {
+    nome: "Bruna",
+    sobrenome: "Leite",
+    setor: "arquibancada",
+    pista: 20
+}
