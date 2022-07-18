47f8bde1ab055ce69d8c767269a5b67f49086a43
ricardo <rcavaraujo@gmail.com>
2022-07-17 22:51:22 -0300

    [M1S03] Ex 9 - Projeto: desafio final

---
+++ ex9.js
@@ -0,0 +1,81 @@
+var convidado1 = {
+    nome: "Ricardo",
+    sobrenome: "Araujo",
+    setor: "camarote",
+    idade: 39
+}
+
+var convidado2 = {
+    nome: "João",
+    sobrenome: "Martins",
+    setor: "arquibancada",
+    idade: 16
+}
+
+var convidado3 = {
+    nome: "Bruna",
+    sobrenome: "Leite",
+    setor: "pista",
+    idade: 20
+}
+
+var listaDeConvidados = [convidado1, convidado2, convidado3]
+
+function liberarBebidas(convidados) {
+    var arrayResultante = [];    
+    for(var i = 0; i < convidados.length; i++) {
+        var objetoDoArray = convidados[i];
+        if(convidados[i].idade > 17) {
+            objetoDoArray.openbar = true;
+        } else {
+            objetoDoArray.openbar = false;
+        }   
+        arrayResultante.push(objetoDoArray)
+    }
+    return arrayResultante; 
+}
+
+var convidadosComBebidasProcessadas = liberarBebidas(listaDeConvidados) 
+
+function separarCamarote(listaDeConvidados){
+    var listaCamarote = listaDeConvidados.filter(elemento => elemento.setor === "camarote")
+    return listaCamarote;
+}
+
+function separarArquibancada(listaDeConvidados){
+    var listaArquibancada = listaDeConvidados.filter(elemento => elemento.setor === "arquibancada")
+    return listaArquibancada;
+}
+
+function separarPista(listaDeConvidados){
+    var listaPista = listaDeConvidados.filter(elemento => elemento.setor === "pista")
+    return listaPista;
+}
+
+var listaCamarote = separarCamarote(convidadosComBebidasProcessadas);
+var listaArquibancada = separarArquibancada(convidadosComBebidasProcessadas);
+var listaPista = separarPista(convidadosComBebidasProcessadas);
+
+var camarote = document.getElementById("listaCamarote")
+var arquibancada = document.getElementById("listaArquibancada")
+var pista = document.getElementById("listaPista")
+
+const montaNome = (objPessoa) => {
+    return `${objPessoa.nome} ${objPessoa.sobrenome}`
+}
+
+const marcarOpenBar = (convidado) => {
+    return convidado.openbar ? "🍹" : "";
+};
+
+listaCamarote.forEach(convidado => {
+    camarote.innerHTML += `<li>${montaNome(convidado)} ${marcarOpenBar(convidado)}</li>`;
+});
+
+listaArquibancada.forEach(convidado => {
+    arquibancada.innerHTML += `<li>${montaNome(convidado)} ${marcarOpenBar(convidado)}</li>`;
+})
+
+listaPista.forEach(convidado => {
+    pista.innerHTML += `<li>${montaNome(convidado)} ${marcarOpenBar(convidado)}</li>`;
+})
